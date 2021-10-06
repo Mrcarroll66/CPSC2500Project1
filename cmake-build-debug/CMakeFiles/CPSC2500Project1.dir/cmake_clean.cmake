576480f20142912020e12e1849c9849b4ce4b93b
Michael Carroll <mrcarroll@anderson.edu>
2021-10-06 10:05:00 -0400

    Project1

---
+++ cmake-build-debug/CMakeFiles/CPSC2500Project1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPSC2500Project1.dir/CreditCard.cpp.o"
+  "CMakeFiles/CPSC2500Project1.dir/main.cpp.o"
+  "CPSC2500Project1.exe"
+  "CPSC2500Project1.pdb"
+  "libCPSC2500Project1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPSC2500Project1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
